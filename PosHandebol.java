7279d2d027e4c72abbadec29cfe412c2a005b54e
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-06-17 15:11:15 -0300

    PosHandebol.java

---
+++ PosHandebol.java
@@ -0,0 +1,18 @@
+
+public enum PosHandebol{
+
+  GOLEIRO(0.8),
+  ARMADOR(1.3),
+  PONTA(1.4),
+  CENTRAL(1.4),
+  PIVO(1.6);
+  
+  Private double gastoPorMinuto;
+
+  PosHandebol(double gastoPorMinuto){
+    this.gastoPorMinuto = gastoPorMinuto;
+  }
+  public double getGastoPorMinuto(){
+    return this.gastoPorMinuto;
+  }
+}
