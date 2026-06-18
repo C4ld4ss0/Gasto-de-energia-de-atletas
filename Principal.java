d806b535b097a4e501be75f58f5a20822b98e385
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-06-18 14:42:44 -0300

    Principal.java

--- Principal.java
+++ Principal.java
@@ -5,19 +5,19 @@ public class Principal{
 
    public static void main(String[] args){
        System.out.println("=== Iniciando sistema da academia ===");
-            processarCatrcaCSV("Treinos.csv");
-            System.out.println("=== FIM DO EXPEDIENTE ===")
+            processarCatracaCSV("Treinos.csv");
+            System.out.println("=== FIM DO EXPEDIENTE ===");
    }
-    public static void processarCatracaCsv(String caminhoArquivo){
+    public static void processarCatracaCSV(String caminhoArquivo){
         try{
             Scanner leitor = new Scanner(new File(caminhoArquivo));
             while(leitor.hasNextLine()){
                 String linha = leitor.nextLine();
-                String[] dados = linha.spli(";");
+                String[] dados = linha.split(";");
                 
                 String nome = dados[0];
                 Condicao cond = Condicao.valueOf(dados[1]);
-                Corpo corp = Corpo.valueOf(dados[2]);
+                Corpo corpo = Corpo.valueOf(dados[2]);
 
                 Atleta atletaDaLinha = new Atleta(nome, cond, corpo);
 
@@ -25,76 +25,83 @@ public class Principal{
                 Treinamento treinoDaLinha = null;
 
                 if (tipoEsporte.equals("FUTEBOL")){
-                    double tempo = double.parseDouble(dados[4]);
-                    PosFutebol pos = posFutebol.valueOf(dados [5]);
+                    double tempo = Double.parseDouble(dados[4]);
+                    PosFutebol pos = PosFutebol.valueOf(dados [5]);
                     int gols = Integer.parseInt(dados [6]);
                     boolean clima = converterParaBoolean(dados[7]);
 
                     treinoDaLinha = new TreinoFutebol(tempo, pos, gols, clima);
                    
                 } else if(tipoEsporte.equals("BASQUETE")){
-                   double tempo = double.parseDouble(dados[4]);
-                   PosBasquete pos = posBasquete.valueOf(dados[5]);
+                   double tempo = Double.parseDouble(dados[4]);
+                   PosBasquete pos = PosBasquete.valueOf(dados[5]);
                    int rebotes = Integer.parseInt(dados[6]);
                    boolean meiaQuadra = converterParaBoolean(dados[7]);
 
                    treinoDaLinha = new TreinoBasquete(tempo, pos, rebotes, meiaQuadra);
                    
                 }else if(tipoEsporte.equals("CORRIDA")){
-                   double distanciaKm = double.parseDouble(dados[4]);
-                   double pace = double.parseDouble(dados[5]);
+                   double distanciaKm = Double.parseDouble(dados[4]);
+                   double pace = Double.parseDouble(dados[5]);
                    
                    treinoDaLinha = new TreinoCorrida(distanciaKm, pace);
                    
                 }else if(tipoEsporte.equals("VÔLEI")){
                    String placar = String.valueOf(dados[4]);
-                   PosVolei pos = posVolei.valueOf(dados[5]);
+                   PosVolei pos = PosVolei.valueOf(dados[5]);
                    
                    treinoDaLinha = new TreinoVolei(placar, pos);
                    
                 }else if(tipoEsporte.equals("HANDEBOL")){
-                   double tempoJogo = double.parseDouble(dados[4]);
-                   PosHandebol pos = posHandebol.valueOf(dados[5]);
+                   double tempoJogo = Double.parseDouble(dados[4]);
+                   PosHandebol pos = PosHandebol.valueOf(dados[5]);
                    int gols = Integer.parseInt(dados[6]);
                    boolean partida = converterParaBoolean(dados[7]);
                    
                    treinoDaLinha = new TreinoHandebol(tempoJogo, pos, gols, partida);
                    
                 }else if(tipoEsporte.equals("ZUMBA")){
-                   double tempo = double.parseDouble(dados[4]);
+                   double tempo = Double.parseDouble(dados[4]);
 
                    treinoDaLinha = new TreinoZumba(tempo);
                    
                 }else if(tipoEsporte.equals("MUSCULAÇÃO")){
-                   int pesoTotalLevantado = int.parseInt(dados[4]);
+                   int pesoTotalLevantado = Integer.parseInt(dados[4]);
 
                    treinoDaLinha = new TreinoMusculacao(pesoTotalLevantado);
                    
                 }else if(tipoEsporte.equals("ATLETISMO")){
-                   double distancia = double.parseDouble(dados[4]);
-                   double tempoMin = double.parseDouble(dados[5]);
+                   double distancia = Double.parseDouble(dados[4]);
+                   double tempoMin = Double.parseDouble(dados[5]);
                    TerrenoCorrida terreno = TerrenoCorrida.valueOf(dados[6]);
                    boolean clima = converterParaBoolean(dados[7]);
                    
                    treinoDaLinha = new TreinoAtletismo(distancia, tempoMin, terreno, clima);
                       
                 }else if(tipoEsporte.equals("FUNCIONAL")){
-                   double tempo = double.parseDouble(dados[4]);
+                   double tempo = Double.parseDouble(dados[4]);
                    NivelFuncional nivel = NivelFuncional.valueOf(dados[5]);
                    boolean pesos = converterParaBoolean(dados[6]);
 
                 treinoDaLinha = new TreinoFuncional(tempo, nivel, pesos);
-                   
-        }catch (FileNotFoundException e){
+                }
+                   if(treinoDaLinha != null){
+                      System.out.println("Catraca liberada: " + nome + " Vai treinar " + tipoEsporte);
+                      atletaDaLinha.treinar(treinoDaLinha);
+                   }else{
+                      System.out.println("Catraca bloqueada: Esporte " + tipoEsporte + " Não reconhecido no sistema.");
+                   }
+            }
+           leitor.close();
+        }
+             catch (FileNotFoundException e){
             System.out.println("Opa! Arquivo não encontrado: " + caminhoArquivo);
         }
-        return listaDeTreinos;
-    }
 
-   public static boolean converterParaBoolean(String texto do CSV){
+   public static boolean converterParaBoolean(String texto){
       if (texto == null){return false;}
    
-      String textoLimpo = texto.trim().toUppercase();
+      String textoLimpo = texto.trim().toUpperCase();
 
       if(textoLimpo.equals("SIM")|| textoLimpo.equals("S") || textoLimpo.equals("TRUE")){return true;}
 
