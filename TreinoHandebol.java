bc2498e3e4ad15f170a0ff4420c8984d8322c499
Artur Besckow Caldasso <124211523+C4ld4ss0@users.noreply.github.com>
2026-06-18 14:40:08 -0300

    TreinoHandebol.java

--- TreinoHandebol.java
+++ TreinoHandebol.java
@@ -1,11 +1,11 @@
 
 public class TreinoHandebol implements Treinamento{
     private double tempoJogo;
-    private posHandebol posicao;
+    private PosHandebol posicao;
     private int golsMarcados;
     private boolean partidaOficial;
 
-    public TreinoHandebol (double tempoJogo, posHandebol posicao, int golsMarcados, boolean partidaOficial){
+    public TreinoHandebol (double tempoJogo, PosHandebol posicao, int golsMarcados, boolean partidaOficial){
         this.tempoJogo = tempoJogo;
         this.posicao = posicao;
         this.golsMarcados = golsMarcados;
@@ -15,10 +15,10 @@ public class TreinoHandebol implements Treinamento{
     @Override
     public int calcularGasto(Corpo corpo){
         double intensidade = 1.0;
-        if(this.partidaOficial){multiplicadorIntensidade = 1.3;}
+        if(this.partidaOficial){intensidade = 1.3;}
         double custoTempo = this.tempoJogo * this.posicao.getGastoPorMinuto() * intensidade;
         custoTempo += this.golsMarcados * 3;
 
-        return (int) (custoBruto * corpo.getMultiplicadorGasto());
+        return (int) (custoTempo * corpo.getMultiplicadorGasto());
     }
 }
